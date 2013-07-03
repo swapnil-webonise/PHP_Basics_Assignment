4423eb35b1e30dcab5d6ba8b69c010f818956ac8
swapnil512 <swapnil.patil@weboniselab.com>
2013-07-03 13:46:06 +0530

    Update arraydemo.php

--- ArrayDemo/arraydemo.php
+++ ArrayDemo/arraydemo.php
@@ -5,18 +5,18 @@
 echo "<b>Array Demo</b>";
 $a=array("item1","item2","item3");
 echo "<br /><br />Single Dimensional Array ";
-echo print_r($a);
+print_r($a);
 $b=array(
 		array("item1","item2","item3"),
 		array("item4","item5","item6"),
 		array("item7","item8","item9")
 		);
 echo "<br /><br />Double Dimensional Array ";
-echo print_r($b);
+print_r($b);
 $c=array("first"=>"item1",
 		 "second"=>"item2",
 		 "third"=>"item3"
 		);
 echo "<br /><br />Associative Array ";
-echo print_r($c);
+print_r($c);
 ?>
